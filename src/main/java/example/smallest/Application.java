b8c30ab3b3a072e07fa705d1ffdcf84dd4554a3d
enenen <ty.subscribe@gmail.com>
2021-02-12 12:17:17 +0100

    Update Application.java

--- src/main/java/example/smallest/Application.java
+++ src/main/java/example/smallest/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+bla 
 
 @ComponentScan
 @EnableAutoConfiguration
